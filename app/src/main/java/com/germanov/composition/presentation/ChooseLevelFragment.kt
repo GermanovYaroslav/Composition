075dc0bb7b476379d2104f6ab1b735aea0f19826
GermanovYaroslav <hierihon@gmail.com>
2024-11-28 17:48:53 +0300

    Implementation navigation

--- app/src/main/java/com/germanov/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/germanov/composition/presentation/ChooseLevelFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.germanov.composition.R
 import com.germanov.composition.databinding.FragmentChooseLevelBinding
 import com.germanov.composition.domain.entity.Level
+import com.germanov.composition.presentation.GameFragment.Companion.KEY_LEVEL
 
 class ChooseLevelFragment : Fragment() {
 
@@ -42,10 +44,10 @@ class ChooseLevelFragment : Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(GameFragment.NAME)
-            .commit()
+        val args = Bundle().apply {
+            putParcelable(KEY_LEVEL, level)
+        }
+        findNavController().navigate(R.id.action_chooseLevelFragment_to_gameFragment, args)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/germanov/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/germanov/composition/presentation/GameFragment.kt
@@ -9,11 +9,13 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.germanov.composition.R
 import com.germanov.composition.databinding.FragmentGameBinding
 import com.germanov.composition.domain.entity.GameResult
 import com.germanov.composition.domain.entity.GameSettings
 import com.germanov.composition.domain.entity.Level
+import com.germanov.composition.presentation.GameFinishedFragment.Companion.KEY_GAME_RESULT
 
 class GameFragment : Fragment() {
 
@@ -120,16 +122,16 @@ class GameFragment : Fragment() {
     }
 
     private fun launchGameFinishedFragment(gameResult: GameResult) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
-            .addToBackStack(null)
-            .commit()
+        val args = Bundle().apply {
+            putParcelable(KEY_GAME_RESULT, gameResult)
+        }
+        findNavController().navigate(R.id.action_gameFragment_to_gameFinishedFragment, args)
     }
 
     companion object {
 
         const val NAME = "GameFragment"
-        private const val KEY_LEVEL = "level"
+        const val KEY_LEVEL = "level"
 
         fun newInstance(level: Level): GameFragment {
             return GameFragment().apply {
